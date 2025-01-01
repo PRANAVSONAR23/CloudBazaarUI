f7c86262d66d4f2cfbf9f945b7ec52b6aa80a433
PRANAVSONAR23 <pranavsonar2311@gmail.com>
2025-01-01 22:08:17 +0530

    added animate plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
@@ -69,6 +72,6 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
 
